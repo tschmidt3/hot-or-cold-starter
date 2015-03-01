7d63be291722f62aaa7ff29615c11983fb79c0de
570514 <toddschmidt@C02L816ZFFT4.local>
2015-03-01 13:31:41 -0500

    added warmer or cooler after first guess and recorded values in console. Also added new Game functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
 	var secretNumber = generateNumber();
+        previousGuess = 101;
+        console.log(secretNumber);
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
             $(".overlay").fadeIn(1000);
@@ -24,10 +26,22 @@ $(document).ready(function(){
         window.onbeforeunload = function() {
             return "refreshing this page will cause the game to start over";
         };
+        //New Game
+        $(".new").click(function(){
+            newGame();
+            secretNumber = generateNumber();
+            
+        });
+        
 });
 
 
 function newGame(){
+    //clear guesses
+    $("ul#guessList").children().remove();
+    //set count to 0
+    $("span#count").text(0);
+    //generate new number
     
 };
 
@@ -35,21 +49,27 @@ function newGame(){
 function generateNumber(){
     var secretNumber = Math.floor((Math.random() * 100) + 1);
     return secretNumber;
-
-    
-}
-
+};
 
 //named function to provide feedback (too high, too low, or just right)
 //reads in the guess and tells if it is an integer, if true passes to the set range, count, and record functions
 function feedback(answer){
+    //want to add functionality that does not allow reguessing
     var guess = $("#userGuess").val();
     if (isInt(guess)){
-        difference(guess, answer);
+        if(previousGuess<100){
+            console.log(previousGuess);
+        }
+        
+        difference(guess, answer, previousGuess);
         //Run counting function
+        count();
         //Run the record function
         record(guess);
-        count();
+        //Store previous guess
+        previousGuess = guess;
+        
+        
     }
     else {
         alert('Please enter and integer');
@@ -57,7 +77,33 @@ function feedback(answer){
 };
 
 //set range of hot vs cold
-function difference(guess, answer){
+function difference(guess, answer, previousGuess){
+    
+    //If first guess (previousGuess = 100 high vs low, if else do hotter vs colder
+    if(previousGuess===101){
+        if (+guess===answer){
+        console.log("correct");
+        }
+        else if (+guess>answer){
+            alert("too high");
+        }
+        else {
+            console.log("too low");
+        }
+    }
+    else {
+        if(+guess===answer){
+            console.log("correct");
+        }
+        else if(Math.abs(guess-answer)>Math.abs(previousGuess-answer)){
+            console.log("colder");
+        }
+        else{
+            console.log("warmer");
+        }
+    }
+    
+    
     
     
 }
