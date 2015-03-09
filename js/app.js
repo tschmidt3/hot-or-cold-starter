9da564fd9e71449c21e6b687f233143d2e149394
Todd Schmidt <tmschmidt527@gmail.com>
2015-03-08 22:18:14 -0400

    added popup windows instead of console.log and added a check to make sure integer guess is between 0 and 100

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 $(document).ready(function(){
 	var secretNumber = generateNumber();
         previousGuess = 101;
-        console.log(secretNumber);
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
             $(".overlay").fadeIn(1000);
@@ -15,7 +14,6 @@ $(document).ready(function(){
         $("#guessButton").click(function(){
             feedback(secretNumber);
         });
-        
         /*---Check Guessed number when enter key is hit ---*/
         $("#userGuess").keyup(function(e){
            if (e.keyCode === 13) { 
@@ -24,33 +22,27 @@ $(document).ready(function(){
             } 
         });
         window.onbeforeunload = function() {
-            return "refreshing this page will cause the game to start over";
+            return "Refreshing this page will cause the game to start over";
         };
         //New Game
         $(".new").click(function(){
-            newGame();
-            secretNumber = generateNumber();
-            
-        });
-        
+            newGame();  
+        });   
 });
-
-
 function newGame(){
     //clear guesses
     $("ul#guessList").children().remove();
     //set count to 0
     $("span#count").text(0);
     //generate new number
+    secretNumber = generateNumber();
     
 };
-
 //named function to generate secret number between 1 and 100
 function generateNumber(){
     var secretNumber = Math.floor((Math.random() * 100) + 1);
     return secretNumber;
 };
-
 //named function to provide feedback (too high, too low, or just right)
 //reads in the guess and tells if it is an integer, if true passes to the set range, count, and record functions
 function feedback(answer){
@@ -60,67 +52,68 @@ function feedback(answer){
         if(previousGuess<100){
             console.log(previousGuess);
         }
-        
-        difference(guess, answer, previousGuess);
-        //Run counting function
-        count();
-        //Run the record function
-        record(guess);
-        //Store previous guess
-        previousGuess = guess;
-        
-        
+        incorrect = difference(guess, answer, previousGuess);
+        if (incorrect){
+            //Run counting function
+            count();
+            //Run the record function
+            record(guess);
+            //Store previous guess
+            previousGuess = guess;
+        }
     }
     else {
-        alert('Please enter and integer');
+        alert('Please enter and integer between 0 and 100');
     };
 };
-
 //set range of hot vs cold
 function difference(guess, answer, previousGuess){
-    
+    incorrect = true;
     //If first guess (previousGuess = 100 high vs low, if else do hotter vs colder
     if(previousGuess===101){
         if (+guess===answer){
-        console.log("correct");
+            incorrect = false;
+            if (confirm('Correct! \nDo you want to play again?')) {
+                newGame();
+            }
         }
         else if (+guess>answer){
             alert("too high");
         }
         else {
-            console.log("too low");
+            alert("too low");
         }
     }
     else {
         if(+guess===answer){
-            console.log("correct");
+            incorrect = false;
+            if (confirm('Do you want to play again?')) {
+                newGame();
+            }
         }
         else if(Math.abs(guess-answer)>Math.abs(previousGuess-answer)){
-            console.log("colder");
+            alert("colder");
         }
         else{
-            console.log("warmer");
+            alert("warmer");
         }
     }
-    
-    
-    
-    
+    return incorrect;   
 }
-
-
 //track how many guesses made
 function count(){
     var temp = $("span#count").text();
     $("span#count").text(+temp+1); 
 }
-
-
 //show the guesses already made
 function record(guess){
     $("ul#guessList").append("<li>"+guess+"</li>");
 }
-
 function isInt(n){
-    return n % 1 === 0;
+    if(n<=100 && n>=0){
+        return n % 1 === 0; 
+    }
+    else{
+        return false;
+    }
 }
